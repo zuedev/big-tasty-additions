7b125cbb17f095fac07c85690cc141e8f2c1dd84
Alex Pooley <zuedev@gmail.com>
2024-11-30 20:47:39 +0000

    split the code!

--- src/main/java/zuedev/bigtastyadditions/BigTastyAdditions.java
+++ src/main/java/zuedev/bigtastyadditions/BigTastyAdditions.java
@@ -1,8 +1,13 @@
 package zuedev.bigtastyadditions;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import zuedev.bigtastyadditions.food.food;
 
 public class BigTastyAdditions implements ModInitializer {
     public static final String MOD_ID = "big-tasty-additions";
@@ -13,6 +18,10 @@ public class BigTastyAdditions implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Hello Fabric world!");
 
-        Items.initialize();
+        food.initialize();
+    }
+
+    public static Item registerItem(Item item, String id) {
+        return Registry.register(Registries.ITEM, new Identifier(BigTastyAdditions.MOD_ID, id), item);
     }
 }
