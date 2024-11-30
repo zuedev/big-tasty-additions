b4c9e5f4101a00d71c03fb584bd692b3858119c0
Alex Pooley <zuedev@gmail.com>
2024-11-30 11:24:23 +0000

    initial commit of fabric template

---
+++ src/client/kotlin/zuedev/bigtastyadditions/BigTastyAdditionsClient.kt
@@ -0,0 +1,9 @@
+package zuedev.bigtastyadditions
+
+import net.fabricmc.api.ClientModInitializer
+
+object BigTastyAdditionsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/zuedev/bigtastyadditions/BigTastyAdditionsDataGenerator.kt
@@ -0,0 +1,9 @@
+package zuedev.bigtastyadditions
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BigTastyAdditionsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
