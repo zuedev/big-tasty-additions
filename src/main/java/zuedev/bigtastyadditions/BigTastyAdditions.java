5ab9dff818f01c21e54e6c6bce5fe6ac89f4c5cd
Alex Pooley <zuedev@gmail.com>
2024-11-30 15:02:15 +0000

    add basically working peeled apple base

---
+++ src/main/java/zuedev/bigtastyadditions/BigTastyAdditions.java
@@ -0,0 +1,37 @@
+package zuedev.bigtastyadditions;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BigTastyAdditions implements ModInitializer {
+    public static final String MOD_ID = "big-tasty-additions";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final Item PEELED_APPLE = registerItem(
+            new Item(
+                    new FabricItemSettings().food(
+                            new FoodComponent.Builder()
+                                    .hunger(4)
+                                    .build()
+                    )
+            ),
+            "peeled_apple"
+    );
+
+    public static Item registerItem(Item item, String id) {
+        return Registry.register(Registries.ITEM, new Identifier(MOD_ID, id), item);
+    }
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
